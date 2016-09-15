72f2c967f9bf58f66bc5e90ddbd45bbb1a5fcd95
Sukhyun Baik <neonagx@Sukhyuns-MacBook-Air.local>
2016-09-14 20:58:12 -0700

    completing assignment 8, 9, 10

---
+++ main.js
@@ -0,0 +1,20 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+if (cardOne === cardThree){
+	alert("You Got a Match!");}
+else if(cardTwo === cardFour){
+	alert("You Got a Match!");
+}
+else{ 
+	alert("Sorry Try Again");
+}
+var board = document.getElementById('game-board');
+function createBoard(){
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
+}}
