7ca30e34342d0dd02604f7e8cc32cb260da4cd4c
Sukhyun Baik <neonagx@Sukhyuns-MacBook-Air.local>
2016-09-19 21:02:49 -0700

    redid assignment 9

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
 if (cardOne === cardThree){
-	alert("You Got a Match!");}
-else if(cardTwo === cardFour){
 	alert("You Got a Match!");
-}
-else{ 
+} else if(cardTwo === cardFour){
+	alert("You Got a Match!");
+} else { 
 	alert("Sorry Try Again");
-}
+};
+
 var board = document.getElementById('game-board');
 function createBoard(){
 	for (var i=0; i<cards.length; i++){
@@ -17,4 +18,4 @@ function createBoard(){
 		cardElement.className = 'card';
 		board.appendChild(cardElement);
 	}
-}}
+}
